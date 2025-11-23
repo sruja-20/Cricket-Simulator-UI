8dfb706dcc356949789f0114b972fbafff5bf49b
Srujan K B <srujankb@Srujans-MacBook-Air.local>
2025-11-23 09:19:26 +0530

    Re organized home component

--- src/shared/components/button/button.component.jsx
+++ src/shared/components/button/button.component.jsx
@@ -1,7 +1,6 @@
 import "./button.component.css";
 
 export default function Button({ onClick, children, ...config }) {
-  console.log(config);
   return (
     <>
       <button {...config} onClick={onClick}>
