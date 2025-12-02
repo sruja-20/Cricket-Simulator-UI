3aa304a48eb4e8c31d03ba24142e66eb56bab425
Srujan K B <srujankb@Srujans-MacBook-Air.local>
2025-12-02 21:30:47 +0530

    Added navigation bar to home page

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,19 +1,12 @@
 import "./home.component.css";
-import QuickPlay from "../quick-play/quick-play.component";
-import AddTeams from "../add-teams/add-teams.component";
-import MatchList from "../match-list/match-list.component";
+import Navigation from "../../shared/components/navigation/navigation.component";
 
 export default function Home() {
   return (
     <>
       <div className="home-container ht-100 lay-column gap8">
-        <div className="lay-row gap8 lay-align-end-end">
-          <QuickPlay />
-          <AddTeams />
-        </div>
-        <div className="layrow matches-container">
-          <MatchList></MatchList>
-        </div>
+        <Navigation></Navigation>
+
       </div>
     </>
   );
